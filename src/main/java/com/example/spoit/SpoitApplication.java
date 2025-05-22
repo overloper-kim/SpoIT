6afc6ed5905957d95934d58f28cc2b5fa3bf46b3
overloper <overloper0225@gmail.com>
2025-05-23 00:54:55 +0900

    first commit

---
+++ src/main/java/com/example/spoit/SpoitApplication.java
@@ -0,0 +1,13 @@
+package com.example.spoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spoit/SpoitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
