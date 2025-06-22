7be882e2640fcc8d5041ab5d9b87a0e1bfadb75d
kwonkyeongwook <sang8431k@naver.com>
2025-06-22 23:15:57 +0900

    search box deleted

--- team.js
+++ team.js
@@ -5,7 +5,8 @@ const teams = {
     color: '#005BAC',
     games: [
       { date: '2024-06-24', time: '18:30', home: true, opponent: 'ssg', location: '대구삼성라이온즈파크', reserveUrl: '#' },
-      { date: '2024-06-25', time: '18:30', home: true, opponent: 'hanwha', location: '대구삼성라이온즈파크', reserveUrl: '#' }
+      { date: '2024-06-25', time: '18:30', home: true, opponent: 'hanwha', location: '대구삼성라이온즈파크', reserveUrl: '#' },
+      { date: '2024-06-30', time: '18:30', home: true, opponent: 'hanwha', location: '대구삼성라이온즈파크', reserveUrl: '#' }
     ]
   },
   ssg: {
@@ -13,7 +14,8 @@ const teams = {
     color: '#E61A23',
     games: [
       { date: '2024-06-26', time: '18:30', home: true, opponent: 'samsung', location: '인천SSG랜더스필드', reserveUrl: '#' },
-      { date: '2024-06-27', time: '18:30', home: true, opponent: 'hanwha', location: '인천SSG랜더스필드', reserveUrl: '#' }
+      { date: '2024-06-27', time: '18:30', home: true, opponent: 'hanwha', location: '인천SSG랜더스필드', reserveUrl: '#' },
+      { date: '2024-06-28', time: '18:30', home: true, opponent: 'samsung', location: '인천SSG랜더스필드', reserveUrl: '#' }
     ]
   },
   hanwha: {
@@ -21,7 +23,8 @@ const teams = {
     color: '#FF6600',
     games: [
       { date: '2024-06-28', time: '18:30', home: true, opponent: 'samsung', location: '대전한화생명이글스파크', reserveUrl: '#' },
-      { date: '2024-06-29', time: '18:30', home: true, opponent: 'ssg', location: '대전한화생명이글스파크', reserveUrl: '#' }
+      { date: '2024-06-29', time: '18:30', home: true, opponent: 'ssg', location: '대전한화생명이글스파크', reserveUrl: '#' },
+      { date: '2024-07-01', time: '18:30', home: true, opponent: 'ssg', location: '대전한화생명이글스파크', reserveUrl: '#' }
     ]
   },
   seoul: {
@@ -29,7 +32,8 @@ const teams = {
     color: '#C60C30',
     games: [
       { date: '2024-07-01', time: '19:00', home: true, opponent: 'jeonbuk', location: '서울월드컵경기장', reserveUrl: '#' },
-      { date: '2024-07-05', time: '19:00', home: true, opponent: 'ulsan', location: '서울월드컵경기장', reserveUrl: '#' }
+      { date: '2024-07-05', time: '19:00', home: true, opponent: 'ulsan', location: '서울월드컵경기장', reserveUrl: '#' },
+      { date: '2024-07-10', time: '19:00', home: true, opponent: 'jeonbuk', location: '서울월드컵경기장', reserveUrl: '#' }
     ]
   },
   jeonbuk: {
@@ -37,7 +41,8 @@ const teams = {
     color: '#006400',
     games: [
       { date: '2024-07-02', time: '19:00', home: true, opponent: 'seoul', location: '전주월드컵경기장', reserveUrl: '#' },
-      { date: '2024-07-06', time: '19:00', home: true, opponent: 'ulsan', location: '전주월드컵경기장', reserveUrl: '#' }
+      { date: '2024-07-06', time: '19:00', home: true, opponent: 'ulsan', location: '전주월드컵경기장', reserveUrl: '#' },
+      { date: '2024-07-11', time: '19:00', home: true, opponent: 'seoul', location: '전주월드컵경기장', reserveUrl: '#' }
     ]
   },
   ulsan: {
@@ -45,7 +50,8 @@ const teams = {
     color: '#1D2088',
     games: [
       { date: '2024-07-03', time: '19:00', home: true, opponent: 'seoul', location: '울산문수축구경기장', reserveUrl: '#' },
-      { date: '2024-07-07', time: '19:00', home: true, opponent: 'jeonbuk', location: '울산문수축구경기장', reserveUrl: '#' }
+      { date: '2024-07-07', time: '19:00', home: true, opponent: 'jeonbuk', location: '울산문수축구경기장', reserveUrl: '#' },
+      { date: '2024-07-12', time: '19:00', home: true, opponent: 'jeonbuk', location: '울산문수축구경기장', reserveUrl: '#' }
     ]
   },
   sk: {
@@ -53,7 +59,8 @@ const teams = {
     color: '#E6002D',
     games: [
       { date: '2024-07-10', time: '19:00', home: true, opponent: 'lg', location: '잠실학생체육관', reserveUrl: '#' },
-      { date: '2024-07-12', time: '19:00', home: true, opponent: 'sono', location: '잠실학생체육관', reserveUrl: '#' }
+      { date: '2024-07-12', time: '19:00', home: true, opponent: 'sono', location: '잠실학생체육관', reserveUrl: '#' },
+      { date: '2024-07-15', time: '19:00', home: true, opponent: 'sono', location: '잠실학생체육관', reserveUrl: '#' }
     ]
   },
   lg: {
@@ -61,15 +68,17 @@ const teams = {
     color: '#1D428A',
     games: [
       { date: '2024-07-11', time: '19:00', home: true, opponent: 'sk', location: '창원실내체육관', reserveUrl: '#' },
-      { date: '2024-07-13', time: '19:00', home: true, opponent: 'sono', location: '창원실내체육관', reserveUrl: '#' }
+      { date: '2024-07-13', time: '19:00', home: true, opponent: 'sono', location: '창원실내체육관', reserveUrl: '#' },
+      { date: '2024-07-16', time: '19:00', home: true, opponent: 'sk', location: '창원실내체육관', reserveUrl: '#' }
     ]
   },
   sono: {
-    name: '고양 소노 스카이거너스',
+    name: '고양 소노 스카이거스',
     color: '#0099D8',
     games: [
       { date: '2024-07-14', time: '19:00', home: true, opponent: 'sk', location: '고양체육관', reserveUrl: '#' },
-      { date: '2024-07-15', time: '19:00', home: true, opponent: 'lg', location: '고양체육관', reserveUrl: '#' }
+      { date: '2024-07-15', time: '19:00', home: true, opponent: 'lg', location: '고양체육관', reserveUrl: '#' },
+      { date: '2024-07-18', time: '19:00', home: true, opponent: 'lg', location: '고양체육관', reserveUrl: '#' }
     ]
   }
 };
